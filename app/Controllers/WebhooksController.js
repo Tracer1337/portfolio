bdd9b22b857b1c8edd8f119e6b7cbee50d2a704e
Merlin Moelter <joebau.0815@googlemail.com>
2020-08-19 18:39:40 +0200

    Add git pull to webhook

--- app/Controllers/WebhooksController.js
+++ app/Controllers/WebhooksController.js
@@ -1,6 +1,11 @@
 const loadProjects = require("../../scripts/load-projects.js")
+const { exec } = require("@m.moelter/task-runner")
 
 async function runLoadProjects(req, res) {
+    // Pull new contents from github
+    await exec("sudo git pull")
+
+    // Load new contents into website
     await loadProjects()
 
     res.end()
