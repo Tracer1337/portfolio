22281103011487e5a50d1164beda276e8aa2d6e0
Merlin Moelter <joebau.0815@googlemail.com>
2020-08-19 18:03:00 +0200

    Fix image modal sizing

--- client/src/components/ImageGrid/Item.js
+++ client/src/components/ImageGrid/Item.js
@@ -2,8 +2,6 @@ import React, { useState, useEffect, useRef, useReducer } from "react"
 import { Card, CardActionArea, Dialog, DialogContent } from "@material-ui/core"
 import { makeStyles } from "@material-ui/core/styles"
 
-import { getDialogImageDimensions } from "../../utils"
-
 const useStyles = makeStyles(theme => ({
     card: {
         marginBottom: theme.spacing(2)
@@ -15,14 +13,28 @@ const useStyles = makeStyles(theme => ({
 
     dialog: {
         alignItems: "center",
-        justifyContent: "center"
+        justifyContent: "center",
+        maxHeight: "unset",
+        height: `calc(100% - ${theme.spacing(8)}px)`,
+        background: "none",
+        boxShadow: "none"
+    },
+
+    innerDialog: {
+        height: "100%",
+        padding: "0 !important",
+        overflow: "hidden"
     },
 
     dialogImage: {
-        // width: "auto",
-        // height: "auto",
         maxWidth: "100%",
-        maxHeight: "100%"
+        maxHeight: "100%",
+        width: "auto",
+        height: "auto",
+        position: "absolute",
+        top: "50%",
+        left: "50%",
+        transform: "translate(-50%, -50%)"
     }
 }))
 
@@ -33,14 +45,17 @@ function Item({ src }) {
     const imageRef = useRef()
 
     const [isDialogOpen, setIsDialogOpen] = useState(false)
-    const [dialogImageDimensions, setDialogImageDimensions] = useState({})
     const [reloadKey, forceReload] = useReducer(key => key + 1, 0)
 
-    useEffect(() => {
-        const resize = async () => {
-            cardRef.current.style.height = imageRef.current.clientHeight + "px"
+    const handleDialogClick = (event) => {
+        if (event.target.tagName === "DIV") {
+            setIsDialogOpen(false)
+        }
+    }
 
-            setDialogImageDimensions(await getDialogImageDimensions(src))
+    useEffect(() => {
+        const resize = () => {
+            cardRef.current.style.height = imageRef.current.clientHeight + "px"
         }
 
         resize()
@@ -63,9 +78,12 @@ function Item({ src }) {
                 onClose={() => setIsDialogOpen(false)}
                 maxWidth="lg"
                 fullWidth
-                classes={{ paper: classes.dialog }}
+                PaperProps={{
+                    onClick: handleDialogClick,
+                    className: classes.dialog
+                }}
             >
-                <DialogContent>
+                <DialogContent classes={{ root: classes.innerDialog }}>
                     <img src={src} alt="" className={classes.dialogImage}/>
                 </DialogContent>
             </Dialog>
