f8af2e7ff99bd22dd585995db7f9ac1569f82f91
Merlin Moelter <joebau.0815@googlemail.com>
2020-08-20 19:56:08 +0200

    Comment cronjob and webhook

--- app/Boot/index.js
+++ app/Boot/index.js
@@ -14,7 +14,7 @@ async function boot() {
         await CIServiceProvider.registerWebhook()
 
         // Automatically update projects
-        new CronJob("0 * * * *", loadProjects, null, true, "Europe/Berlin").start()
+        // new CronJob("0 * * * *", loadProjects, null, true, "Europe/Berlin").start()
     } else {
         // Update projects whenever content folder has changed
         let isActive = false
