097776fb6b3952fbd29276daa00e73a0cde49acf
Tracer1337 <merlin.moelter@gmail.com>
2022-04-30 17:42:59 +0200

    Create empty next app

--- app/Boot/index.js
+++
@@ -1,7 +0,0 @@
-const ContentLoader = require("../Services/ContentLoader")
-
-async function boot() {
-    await ContentLoader.loadContent()
-}
-
-module.exports = boot
--- content/projects/shitty-search/README.md
+++
@@ -1,1 +0,0 @@
-Shitty Search is a simple search engine which may never be used by anyone.
