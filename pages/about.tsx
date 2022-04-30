35b19559da70f538f76dd9235f4d640a92135daf
Tracer1337 <merlin.moelter@gmail.com>
2022-04-30 22:20:25 +0200

    Remove dummy content

--- pages/about.tsx
+++
@@ -1,7 +0,0 @@
-/** @jsxImportSource @emotion/react */
-
-export default function About() {
-    return (
-        <div>Hallo</div>
-    )
-}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,8 @@
 /** @jsxImportSource @emotion/react */
-import { css } from "@emotion/react"
-import Link from "next/link"
-import Button from "../components/styled/Button"
 
 export default function Index() {
     return (
         <>
-            <Link href="/about">
-                <Button css={css`margin-bottom: 1000px`}>
-                    Click Me
-                </Button>
-            </Link>
         </>
     )
 }
