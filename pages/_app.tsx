fa06d3f50ee08849968b90cd0ebbb53a4eb63002
Tracer1337 <merlin.moelter@gmail.com>
2022-04-30 18:39:47 +0200

    Integrate mui

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,25 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import * as React from "react"
+import Head from "next/head"
+import { ThemeProvider } from "@mui/material/styles"
+import CssBaseline from "@mui/material/CssBaseline"
+import { CacheProvider } from "@emotion/react"
+import theme from "../lib/theme"
+import { createEmotionCache } from "../lib/emotion"
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+const clientSideEmotionCache = createEmotionCache()
+
+export default function App(props: any) {
+    const { Component, emotionCache = clientSideEmotionCache, pageProps } = props
+
+    return (
+        <CacheProvider value={emotionCache}>
+            <Head>
+                <meta name="viewport" content="initial-scale=1, width=device-width" />
+            </Head>
+            <ThemeProvider theme={theme}>
+                <CssBaseline />
+                <Component {...pageProps} />
+            </ThemeProvider>
+        </CacheProvider>
+    )
 }
-
-export default MyApp
---
+++ pages/_document.tsx
@@ -0,0 +1,58 @@
+import * as React from "react"
+import Document, { Html, Head, Main, NextScript } from "next/document"
+import createEmotionServer from "@emotion/server/create-instance"
+import theme from "../lib/theme"
+import { createEmotionCache } from "../lib/emotion"
+
+export default class MyDocument extends Document {
+    render() {
+        return (
+            <Html lang="en">
+                <Head>
+                    <meta name="theme-color" content={theme.palette.primary.main} />
+                    <link rel="shortcut icon" href="/static/favicon.ico" />
+                    <link rel="preconnect" href="https://fonts.googleapis.com" />
+                    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+                    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;900&display=swap" rel="stylesheet" />
+                    {/* @ts-ignore */}
+                    {this.props.emotionStyleTags}
+                </Head>
+                <body>
+                    <Main />
+                    <NextScript />
+                </body>
+            </Html>
+        )
+    }
+}
+
+MyDocument.getInitialProps = async (ctx) => {
+    const originalRenderPage = ctx.renderPage
+
+    const cache = createEmotionCache()
+    const { extractCriticalToChunks } = createEmotionServer(cache)
+
+    ctx.renderPage = () =>
+        originalRenderPage({
+            enhanceApp: (App) => function EnhanceApp(props) {
+                // @ts-ignore
+                return <App emotionCache={cache} {...props} />
+            },
+        })
+
+    const initialProps = await Document.getInitialProps(ctx)
+    const emotionStyles = extractCriticalToChunks(initialProps.html)
+    const emotionStyleTags = emotionStyles.styles.map((style) => (
+        <style
+            data-emotion={`${style.key} ${style.ids.join(" ")}`}
+            key={style.key}
+            // eslint-disable-next-line react/no-danger
+            dangerouslySetInnerHTML={{ __html: style.css }}
+        />
+    ))
+
+    return {
+        ...initialProps,
+        emotionStyleTags,
+    }
+}
