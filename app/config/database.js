99164eddb6661d9c1484a32b20c76eaa2b3788f8
Tracer1337 <merlin.moelter@gmail.com>
2022-05-02 18:02:48 +0200

    feat: use mysql database

--- app/config/database.js
+++ app/config/database.js
@@ -1,10 +1,12 @@
-const path = require('path');
-
 module.exports = ({ env }) => ({
   connection: {
-    client: 'sqlite',
+    client: 'mysql',
     connection: {
-      filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
+      host: env('DATABASE_HOST', 'localhost'),
+      port: parseInt(env('DATABASE_PORT', 3306)),
+      database: env('DATABASE_NAME', 'portfolio'),
+      user: env('DATABASE_USER', 'root'),
+      password: env('DATABASE_PASSWORD', 'root'),
     },
     useNullAsDefault: true,
   },
