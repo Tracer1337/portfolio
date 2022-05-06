e371dde5a593a20bd283202e216f23295fbbfa85
Tracer1337 <merlin.moelter@gmail.com>
2022-05-06 13:06:10 +0200

    fix: remove standard favicon

--- client/pages/_document.tsx
+++ client/pages/_document.tsx
@@ -16,7 +16,6 @@ function MyDocument(props: React.ComponentProps<typeof Document> & {
     return (
         <Html lang="en">
             <Head>
-                <link rel="shortcut icon" href="/favicon.ico" />
                 <link rel="preconnect" href="https://fonts.googleapis.com" />
                 <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
                 <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;900&display=swap" rel="stylesheet" />
